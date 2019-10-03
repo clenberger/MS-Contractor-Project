7d82cc57954917b1097b1a7fded90189b4ac442b
clenberger <christian.lenberger@gmail.com>
2019-10-03 11:42:59 -0700

    Added templates/hoodies_index.html to create an index(bootstrap list view possibly) that allows users to view the stock of hoodies, tried google fonts but was unsuccessful(wait until later)

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return render_template('home.html', msg='Hoody.')
+
+hoodies = [
+    { 'title': 'Original Logo', 'description': 'Original \"Hoody.\" logo' },
+    { 'title': 'New Wave', 'description': 'The new classic!' }
+]
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def hoodies_index():
+    """Show all hoodies."""
+    return render_template('hoodies_index.html', hoodies=hoodies)
 
 if __name__ == '__main__':
     app.run(debug=True)
