d62b5cbaf676936f0b10cee55209fd1efd3d5a8a
clenberger <christian.lenberger@gmail.com>
2019-10-03 12:26:54 -0700

    Worked on route for templates/hoodie_index.html

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('home.html', msg='Hoody.')
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html')
 
 hoodies = [
     { 'title': 'Original Logo', 'description': 'Original \"Hoody.\" logo' },
     { 'title': 'New Wave', 'description': 'The new classic!' }
 ]
 
-@app.route('/')
+@app.route('/hoodies')
 def hoodies_index():
     """Show all hoodies."""
     return render_template('hoodies_index.html', hoodies=hoodies)
+
+@app.route('/hoodies/<hoodie_id>')
+def single_hoodie_view(hoodie_id):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
